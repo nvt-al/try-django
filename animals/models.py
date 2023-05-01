3de79908cb4426175363ee9763755853d5005728
Alexander <alex_nav@gmail.com>
2023-05-01 08:17:23 +0500

    add migrate

--- animals/models.py
+++ animals/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Animals(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField(blank=True)
+    photo = models.ImageField(upload_to="photos/%Y/%m/%d/")
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+    time_published = models.BooleanField(default=True)
