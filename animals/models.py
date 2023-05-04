abc46ee51c1d1595b16e51b18d1a72addb7238a4
Alexander <alex_nav@gmail.com>
2023-05-04 17:49:47 +0500

    added migration and db

--- animals/models.py
+++ animals/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Animals(models.Model):
@@ -8,9 +9,25 @@ class Animals(models.Model):
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
     time_published = models.BooleanField(default=True)
-
+    cat = models.ForeignKey(
+        "Category", on_delete=models.PROTECT, null=True
+    )  # связь Many to One
+    # запрещает удалять категории, на которые есть ссылки из модели
 
     def __str__(self):
         return self.title
 
+    # генерация ссылок на объекты из бд
+    def get_absolute_url(self):
+        return reverse("post", kwargs={"post_id": self.pk})
 
+
+# модель Category, которая представляет категории животных
+class Category(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("category", kwargs={"cat_id": self.pk})
--- animals/views.py
+++ animals/views.py
@@ -1,31 +1,65 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect
 
-from animals.models  import *
+from animals.models import *
 
-menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
+menu = [
+    {"title": "О сайте", "url_name": "about"},
+    {"title": "Добавить статью", "url_name": "add_page"},
+    {"title": "Обратная связь", "url_name": "contact"},
+    {"title": "Войти", "url_name": "login"},
+]
 
 
 def index(request):
     posts = Animals.objects.all()
-    return render(request, "animals/index.html", {'posts': posts, 'menu': menu, 'title': 'Главная страница'})
+    cats = Category.objects.all()
+
+    context = {
+        "posts": posts,
+        "cats": cats,
+        "menu": menu,
+        "title": "Главная страница",
+        "cat_selected": 0,
+    }
+    return render(request, "animals/index.html", context=context)
 
 
 def about(request):
-    return render(request, "animals/about.html", {'menu': menu, 'title': 'О сайте'})
+    return render(request, "animals/about.html", {"menu": menu, "title": "О сайте"})
 
 
-def categories(request, categories_id):
-    if request.GET:
-        print(request.GET)
-    return HttpResponse(f"<h1> Категории </h1><p>{categories_id}</p>")
+def addpage(request):
+    return HttpResponse("Добавление статьи")
 
 
-def archive(reques, year):
-    if int(year) > 2020:
-        return redirect("home", permanent=True)
+def contact(request):
+    return HttpResponse("Обратная связь")
 
-    return HttpResponse(f"<h1>Архив по годам</h1></p>{year}</p>")
+
+def login(request):
+    return HttpResponse("Авторизация")
+
+
+def show_post(request, post_id):
+    return HttpResponse(f"Отображение статьи с id = {post_id}")
+
+
+def show_category(request, cat_id):
+    posts = Animals.objects.filter(cat_id=cat_id)
+    cats = Category.objects.all()
+
+    if len(posts) == 0:
+        raise Http404()
+
+    context = {
+        "posts": posts,
+        "cats": cats,
+        "menu": menu,
+        "title": "Отображение по категориям",
+        "cat_selected": cat_id,
+    }
+    return render(request, "animals/index.html", context=context)
 
 
 def pageNotFound(request, exception):
