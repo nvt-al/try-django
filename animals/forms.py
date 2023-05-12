2db729f355fc97375d1d35e904a95df0b1c33a76
Alexander <alex_nav@gmail.com>
2023-05-12 17:36:14 +0500

    added authorization

--- animals/forms.py
+++ animals/forms.py
@@ -1,7 +1,9 @@
 from django import forms
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.forms import UserCreationForm
 from django.forms import ValidationError
+
 
 from .models import *
 
@@ -50,3 +52,9 @@ class RegisterUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ("username", "email", "password1", "password2")
+
+
+
+class LoginUserForm(AuthenticationForm):
+    username = forms.CharField(label='Логин', widget=forms.TextInput(attrs={'class': 'form-input'}))
+    password = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
--- animals/views.py
+++ animals/views.py
@@ -1,10 +1,12 @@
-from typing import Any, Dict
-from django.db.models.query import QuerySet
 from django.http import HttpResponse, HttpResponseNotFound, Http404
-from django.shortcuts import get_object_or_404, render, redirect
+from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
 from django.views.generic import DetailView, ListView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView
+from django.contrib.auth import logout
+from django.contrib.auth import login
+
 
 from .forms import *
 from .models import *
@@ -27,10 +29,6 @@ class AnimalsHome(DataMixin, ListView):  # отвечает за главную 
         return Animals.objects.filter(time_published=True)
 
 
-def about(request):
-    return render(request, "animals/about.html", {"menu": menu, "title": "О сайте"})
-
-
 class AddPage(
     DataMixin,
     CreateView,
@@ -45,14 +43,6 @@ class AddPage(
         context = super().get_context_data(**kwargs)
         c_def = self.get_user_context(title="Добавление статьи")
         return dict(list(context.items()) + list(c_def.items()))
-
-
-def contact(request):
-    return HttpResponse("Обратная связь")
-
-
-def login(request):
-    return HttpResponse("Авторизация")
 
 
 class ShowPost(
@@ -86,19 +76,49 @@ class AnimalsCategory(DataMixin, ListView):  # страница, предста�
         context["menu"] = menu
         context["cat_selected"] = context["posts"][0].cat_id
         return context
-    
+
 
 class RegisterUser(DataMixin, CreateView):
     form_class = RegisterUserForm
-    template_name = 'animals/register.html'
-    success_url = reverse_lazy('login')
+    template_name = "animals/register.html"
+    success_url = reverse_lazy("login")
 
-    
     def get_context_data(self, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        c_def = self.get_user_context(title='Регистрация')
+        c_def = self.get_user_context(title="Регистрация")
         return dict(list(context.items()) + list(c_def.items()))
+
+    def form_valid(self, form):  # валидация при регистрации и авторизация 2 in 1
+        user = form.save()
+        login(self.request, user)
+        return redirect("home")
+
+
+class LoginUser(DataMixin, LoginView):
+    form_class = LoginUserForm
+    template_name = "animals/login.html"
+
+    def get_context_data(self, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title="Авторизация")
+        return dict(list(context.items()) + list(c_def.items()))
+
+    def get_success_url(self):  # валидация формы и реверс
+        return reverse_lazy("home")
+
+
+def logout_user(request):  # выйти и вернуться на авторизацию
+    logout(request)
+    return redirect("login")
 
 
 def pageNotFound(request, exception):
     return HttpResponseNotFound("<h1> Страница не найдена</h1>")
+
+
+def contact(request):
+    return HttpResponse("Обратная связь")
+
+
+def about(request):
+    return render(request, "animals/about.html", {"menu": menu, "title": "О сайте"})
